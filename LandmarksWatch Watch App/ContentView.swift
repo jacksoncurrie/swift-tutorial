c84f6f472a2860b21d522bf35fa3b71c6faa89a7
Jackson Currie <jackson.currie@bluelab.com>
2023-12-19 16:24:23 +1300

    Add watch app

---
+++ LandmarksWatch Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LandmarksWatch Watch App
+//
+//  Created by Jackson Currie on 19/12/23.
+//
+
+import SwiftUI
+import UserNotifications
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .task {
+                let center = UNUserNotificationCenter.current()
+                _ = try? await center.requestAuthorization(
+                    options: [.alert, .sound, .badge]
+                )
+            }
+    }
+}
+
+#Preview {
+    ContentView()
+        .environment(ModelData())
+}
